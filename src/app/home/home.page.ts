090906dc5e3f107dcfe60659c24b0263dbbf8b2a
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-12-18 23:31:06 -0500

    init commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  imageSource:any;
   constructor() {}
 
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt
+    });
+
+    this.imageSource = image.dataUrl;
+  }
 }
